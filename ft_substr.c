60ed46f365b8f444b816c2e4f6c50f3ae2aa9740
hyeonkki <hyeonkki@student.42seoul.kr>
2021-05-06 13:43:43 +0900

    1

--- ft_substr.c
+++ ft_substr.c
@@ -15,16 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	int		i;
+	unsigned int		i;
 
-	i = -1;
+	i = 0;
 	if (start > ft_strlen((char *)s))
 		return (0);
 	str = (char *)malloc((len + 1) * sizeof(char));
 	if (str == 0)
 		return (0);
-	while (++i < (int)len && s[start + i])
+	while (i < (int)len && s[start + i])
+	{
 		str[i] = s[start + i];
+		i++;
+	}
 	str[i] = 0;
 	return (str);
 }
